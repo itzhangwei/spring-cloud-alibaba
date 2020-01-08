3cf43f5825a63c90af4268834a8489790322a222
zack.zhang <zhangwei>
2020-01-08 17:44:17 +0800

    添加gateway模块

---
+++ gateway/src/main/java/com/learn/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.learn.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhang
+ * @projectName spring-cloud-alibaba
+ * @title GatewayApplication
+ * @package com.learn.cloud.gateway
+ * @description 网管
+ * @date 2020/1/7 7:10 下午
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
